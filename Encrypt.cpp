e99df2f3184a20f4ecda62f8f95c52c377aca241
Yushu Cao <caoyushu@plus.seas.upenn.edu>
2013-04-25 23:49:12 -0400

    Add Decryption

--- Encrypt.cpp
+++ Encrypt.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string>
+#include <string.h>
 #include <iostream>
 
 using namespace std;
@@ -19,9 +20,11 @@ int Encrypt::encrypt_char(int msg, long e, long c) {
   return rsa.modulo(msg, e, c);
 }
 
-int Encrypt::decrypt_char(int msg, long d, long c) {
+int Encrypt::decrypt_char(int msg) {
   MiniRSA rsa;
-  return rsa.modulo(msg, d, c);
+  int res = rsa.modulo(msg, D, C);
+  cout << (char) res << " " << res;
+  return res;
 }
 
 int * Encrypt::encrypt_sen(string msg) {
@@ -40,6 +43,10 @@ void Encrypt::set_public(long e, long c) {
   C = c;
 }
 
+void Encrypt::set_private(long d, long c) {
+  D = d;
+  C = c;
+}
 int main(int argc, char** argv) {
   cout << "Please enter the public key (e, c): first e, then c" << endl;
   string line;
@@ -57,4 +64,23 @@ int main(int argc, char** argv) {
   cout << "Please enter a sentence to encrypt" << endl;
   getline(cin, line);
   encrypt.encrypt_sen(line);
+  cout << "Please enter the private key (d, c): first d, then c" << endl;
+  getline(cin, line);
+
+  p1 = line.find(" ", 0);
+  if (p1 == string::npos) {
+    cout << "Please enter a white space between two numbers" << endl;
+    exit(0);
+  }
+  first_num = atoi(line.substr(0, p1).c_str());
+  second_num = atoi(line.substr(p1 + 1, string::npos).c_str());
+  encrypt.set_private(first_num, second_num);
+  while (1) {
+    cout << "Enter next char cipher value as an int, type quit to quit" << endl;
+    getline(cin, line);
+    if (strcmp(line.c_str(), "quit") == 0) { 
+      break;
+    }
+    encrypt.decrypt_char(atoi(line.c_str()));
+  }
 }
--- Encrypt.h
+++ Encrypt.h
@@ -10,12 +10,15 @@ class Encrypt {
   Encrypt();
   Encrypt(long e, long c);
   void set_public(long e, long c);
+  void set_private(long d, long c);
   int encrypt_char(int msg, long e, long c);
-  int decrypt_char(int msg, long d, long c);
+  int decrypt_char(int msg);
   int * encrypt_sen(string msg);
+  
  private:
   long E;
   long C;
+  long D;
   
   
 };
